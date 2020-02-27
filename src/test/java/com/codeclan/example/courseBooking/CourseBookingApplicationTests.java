b5cce8dfe60f4f6e6db99e3d6258f4959fa38656
Stephen Ramsay <rabrandom@gmail.com>
2020-02-27 10:43:31 +0000

    initial commit

---
+++ src/test/java/com/codeclan/example/courseBooking/CourseBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.courseBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
